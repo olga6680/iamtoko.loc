014ccba6c792582a16c5eb2f1ecf31c9d71f34d5
Olga <boa906680@gmail.com>
2021-09-22 23:21:33 +0300

    add module newsletter-subscribe

---
+++ catalog/language/ru-ru/extension/module/newsletter.php
@@ -0,0 +1,12 @@
+<?php
+// Heading
+$_['heading_title']     = 'Подписаться на рассылку';
+$_['entry_email']     = 'Email';
+$_['button_subscribe']     = 'Подписаться';
+$_['button_unsubscribe']     = 'Отписаться';
+$_['error_email']     = 'Неправильный Email';
+$_['message_not_found']     = 'Указанного email нет в списке подписчиков';
+$_['message_unsubscribed']     = 'Вы отписались от подписки.';
+$_['message_exist']     = 'Спасибо за подписку :)';
+
+$_['success_message']     = 'Спасибо за подписку :)';
---
+++ catalog/language/ua-uk/extension/module/newsletter.php
@@ -0,0 +1,12 @@
+<?php
+// Heading
+$_['heading_title']     = 'Підписатись на розсилку';
+$_['entry_email']     = 'Email';
+$_['button_subscribe']     = 'Підписатись';
+$_['button_unsubscribe']     = 'Відписатись';
+$_['error_email']     = 'Невірний Email';
+$_['message_not_found']     = 'Цього email немає в списку підписників';
+$_['message_unsubscribed']     = 'Ви відписались від підписки.';
+$_['message_exist']     = 'Дякуємо за підписку :)';
+
+$_['success_message']     = 'Дякуємо за підписку :)';

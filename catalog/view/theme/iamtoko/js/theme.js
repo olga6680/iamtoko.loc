a6f319a57a35438d3051620b2293455e81d4569d
Olga <boa906680@gmail.com>
2021-10-13 11:54:04 +0300

    add news and advantages

--- catalog/view/theme/iamtoko/js/theme.js
+++ catalog/view/theme/iamtoko/js/theme.js
@@ -1,3 +1,11 @@
+function getCookie(name) {
+    var matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu'),
         html = document.querySelector('html'),
@@ -19,7 +27,6 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-
 /*фиксированное меню */
 var $menu = $(".header__nav");
 
@@ -31,86 +38,195 @@ $(window).scroll(function() {
     }
 });
 
+$(function() {
 
 
+    $('#slideshow0').swiper({
+        mode: 'horizontal',
+        effect: 'fade',
+        autoplay: 10000,
+        autoplayDisableOnInteraction: true,
+        loop: true
+    });
+
+    /*--------- слайдер student ---*/
+    $('#slideshow1').swiper({
+        mode: 'horizontal',
+        autoplay: 5000,
+        autoplayDisableOnInteraction: true,
+        loop: true
+    });
+
+    /*--------- витрина на главной ---*/
+
+    $('.swiper-container1').swiper({
+        mode: 'horizontal',
+        autoplay: 30000,
+        centeredSlides: true,
+        spaceBetween: 10,
+        loop: true,
+        //pagination: '.carousel{{ module }}',
+        pagination: false,
+        paginationClickable: false,
+        prevButton: '.swiper-button-prev',
+        nextButton: '.swiper-button-next',
+
+        // Default parameters
+        slidesPerView: 5,
+
+        // Responsive breakpoints
+        breakpoints: {
+            // when window width is <= 1200px
+            1200: {
+                slidesPerView: 3
+            },
+            // when window width is <= 575px
+            575: {
+                slidesPerView: 1
+            }
+        }
+
+    });
+
+    $('#content .category-content-thumb .product-layout:nth-child(8)').after($('#product-category .banner-pro'));
 
 
-/*--------- большой слайдер на главной ---*/
-$('#slideshow0').swiper({
-    mode: 'horizontal',
-    effect: 'fade',
-    autoplay: 100000,
-    autoplayDisableOnInteraction: true,
-    loop: true
-});
+    //Sale popup и Email popup
+    $('.button-sale-popup, .button-news-popup').magnificPopup({
+        mainClass: 'mfp-zoom-in',
+        removalDelay: 500
+    });
 
-/*--------- слайдер student ---*/
-$('#slideshow1').swiper({
-    mode: 'horizontal',
-    autoplay: 5000,
-    autoplayDisableOnInteraction: true,
-    loop: true
-});
+    $('.button-salepopup').on('click', function() {
+        $('.sale-popup, .news-popup').fadeOut();
+        $.magnificPopup.close();
+    });
 
+    $('.button-newspopup, .button-text-home-center').on('click', function() {
+        $('.sale-popup, .news-popup').fadeOut();
+        $('.form-news-popup').fadeIn();
+    });
 
+    $('.newsletter_box input[name=\'email\']').bind('keydown', function(e) {
+        if (e.keyCode == 13) {
+            $('#form-newspopup #button-subscribe, #button-subscribe').trigger('click');
+        }
+    });
 
+    $('#form-newspopup #button-subscribe, #button-subscribe').on('click', function() {
+        var success_message = $('#success_message').val()
+        $.ajax({
+            url: 'index.php?route=product/category/subscribe',
+            type: 'post',
+            dataType: 'json',
+            data: 'email=' + encodeURIComponent($('input[id=\'email\']').val()),
+            beforeSend: function() {
+                $('#form-newspopup #button-subscribe, #button-subscribe').button('loading');
+            },
+            complete: function() {
+                $('#form-newspopup #button-subscribe, #button-subscribe').button('reset');
+            },
+            success: function(json) {
+                $('.text-success, .text-danger').remove();
+                if (json['error']) {
+                    $('#respond').after('<div class="text-success text-danger"><i class="fa fa-exclamation-circle"></i> ' + json['error'] + '</div>');
+                    setTimeout(function() {
+                        $('.text-danger').fadeOut('fast');
+                    }, 2000);
+                }
+                if (json['success']) {
+                    $('#respond').after('<div class="text-success"><i class="fa fa-check-circle"></i> ' + success_message + '</div>');
+                    $.cookie('newsletter_already_added', 1, { expires: 14, path: '/' });
+                    setTimeout(function() {
+                        $.magnificPopup.close();
+                    }, 1900);
+                    $('input[id=\'email\']').val('');
+                    setTimeout(function() {
+                        $('.text-success').fadeOut('fast');
+                    }, 2000);
+                }
+            }
+        });
+    });
 
-/*--------- активный пункт меню  ---*/
+    $('#form-callbackfooter-footer #button_send_callbackfooter_footer').on('click', function() {
+        var chatid = "1733030241";
+        var token = "1719065031:AAEyWvsbYXutbKYssfV74KqapJz83KC3hxg";
+        var text = "Текст для <b>нашего</b> бота";
+        otpravka(token, text, chatid);
 
-jQuery(document).ready(function($) {
-    var url = document.location.href;
-    $.each($(".menu a"), function() {
-        if (this.href == url) {
-            $(this).addClass('active');
+        function otpravka(token, text, chatid) {
+            var z = $.ajax({
+                type: "POST",
+                url: "https://api.telegram.org/bot" + token + "/sendMessage?chat_id=" + chatid,
+                data: "parse_mode=HTML&text=" + encodeURIComponent(text),
+            });
         };
     });
-});
 
-$('#form-callbackfooter-footer #button_send_callbackfooter_footer').on('click', function() {
-    var chatid = "1733030241";
-    var token = "1719065031:AAEyWvsbYXutbKYssfV74KqapJz83KC3hxg";
-    var text = "Текст для <b>нашего</b> бота";
-    otpravka(token, text, chatid);
+    $('.button-sale-popup, .button-news-popup').on('click', function() {
+        $('.sale-popup, .news-popup').fadeIn();
+    });
 
-    function otpravka(token, text, chatid) {
-        var z = $.ajax({
-            type: "POST",
-            url: "https://api.telegram.org/bot" + token + "/sendMessage?chat_id=" + chatid,
-            data: "parse_mode=HTML&text=" + encodeURIComponent(text),
+    $('.clos-sort, .btn-menu-menu, .btn-contact-menu').click(function() {
+        $(this).toggleClass('clos-sort-plus');
+    });
+
+    //4 фото в категориях
+    $('.category-content-thumb > .product-layout > .product-thumb').each(function(e) {
+
+        e += 1;
+
+        $(this).parent().attr({
+            'class': 'product-layout product-grid col-lg-3 col-md-4 col-sm-6 col-xs-6 data-bal="element-bal"'
+
         });
-    };
+    });
+
+
+    $('.swiper-slide > .rewiev-wrapper > .caption > .text-rewiev, .blog-blog > .row > #content > .blog > .row > .blog-item > .summary > p').css('height', '').equalHeights();
+
+    //Купить в один клик
+    $('.product-layout > .product-thumb').each(function(e) {
+
+
+        var $easyzoom = $('.easyzoom').easyZoom();
+        // Setup thumbnails example
+        var api1 = $easyzoom.filter('.easyzoom--with-thumbnails').data('easyZoom');
+
+        $('.thumbnails').on('click', 'a', function(e) {
+            var $this = $(this);
+            e.preventDefault();
+            // Use EasyZoom's `swap` method
+            api1.swap($this.data('standard'), $this.attr('href'));
+        });
+
+    });
+
+
+    $('.product-thumb h4, .latest-blog p, .pm-review-thumb .caption .text-review').css('height', '').equalHeights();
+
+    // при нажатии на кнопку scrollup
+    $('#product-product #product .form-group:nth-child(3) .img-thumbnail, .button-scroll, .scrollup').click(function() {
+        // переместиться в верхнюю часть страницы
+        $("html, body").animate({
+            scrollTop: 0
+        }, 1000);
+    });
+
+
 });
 
-$('.swiper-container1').swiper({
-    mode: 'horizontal',
-    autoplay: 30000,
-    centeredSlides: true,
-    spaceBetween: 10,
-    loop: true,
-    //pagination: '.carousel{{ module }}',
-    pagination: false,
-    paginationClickable: false,
-    prevButton: '.swiper-button-prev',
-    nextButton: '.swiper-button-next',
-
-    // Default parameters
-    slidesPerView: 5,
-
-    // Responsive breakpoints
-    breakpoints: {
-        // when window width is <= 1200px
-        1200: {
-            slidesPerView: 3
-        },
-        // when window width is <= 575px
-        575: {
-            slidesPerView: 1
-        }
+// при прокрутке окна (window)
+$(window).scroll(function() {
+    // если пользователь прокрутил страницу более чем на 200px
+    if ($(this).scrollTop() > 400) {
+        // то сделать кнопку scrollup видимой
+        $('.scrollup, .button-coll-side').fadeIn();
+    }
+    // иначе скрыть кнопку scrollup
+    else {
+        $('.scrollup, .button-coll-side').fadeOut();
     }
 
-});
-
-$(function() {
-    $('.product-thumb h4, .latest-blog p, .pm-review-thumb .caption .text-review').css('height', '').equalHeights();
-    $('.swiper-slide > .rewiev-wrapper > .caption > .text-rewiev, .blog-blog > .row > #content > .blog > .row > .blog-item > .summary > p').css('height', '').equalHeights();
 });
